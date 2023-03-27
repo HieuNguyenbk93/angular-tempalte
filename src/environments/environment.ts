e93073c5ce5f4e5d46cb628a0f89a5790703935d
Hieu Nguyen <hieunguyenbk93@gmail.com>
2023-03-27 15:49:49 +0700

    add screen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDK8PNgEdXiCpuk-WrJ0FCmmOX0Nom1DcU',
+    authDomain: 'join-football-team.firebaseapp.com',
+    projectId: 'join-football-team',
+    storageBucket: 'join-football-team.appspot.com',
+    messagingSenderId: '645095205474',
+    appId: '1:645095205474:web:90abee1b80b0cdfb9c3f3b',
+    measurementId: 'G-C3YNY6LFG4',
+  },
 };
 
 /*
